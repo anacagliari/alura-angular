f108c97b40723fa68e5dc468ce232feaa47f585b
Ana Cagliari <111251330+anacagliari@users.noreply.github.com>
2025-04-15 16:31:58 -0300

    aulra-angular-crud-aula04

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -7,6 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListarPensamentoComponent implements OnInit {
 
+  listaPensamentos =[
+    {
+      conteudo: 'Sua tarefa é descobrir o seu trabalho e, então, com todo o coração, dedicar-se a ele.',
+      autoria: 'Buda',
+      modelo: 'modelo3'
+    },
+    {
+      conteudo: 'Devíamos ser ensinados a não esperar por inspiração para começar algo. Ação sempre gera inspiração. Inspiração raramente gera ação.',
+      autoria: 'Frank Tibolt',
+      modelo: 'modelo2'
+    },
+    {
+      conteudo: 'Você Mesmo: Lembre-se de que você mesmo é o melhor secretário de sua tarefa, o mais eficiente propagandista de seus ideais, a mais clara demonstração de seus princípios, o mais alto padrão do ensino superior que seu espírito abraça e a mensagem viva das elevadas noções que você transmite aos outros. Não se esqueça, igualmente, de que o maior inimigo de suas realizações mais nobres, a completa ou incompleta negação do idealismo sublime que você apregoa, a nota discordante da sinfonia do bem que pretende executar, o arquiteto de suas aflições e o destruidor de suas oportunidades de elevação - é você mesmo.',
+      autoria: 'Chico Xavier',
+      modelo: 'modelo1'
+    },
+    {
+      conteudo: 'Cada dia a natureza produz o suficiente para nossa carência. Se cada um tomasse o que lhe fosse necessário, não havia pobreza no mundo e ninguém morreria de fome.',
+      autoria: 'Martin Luther King',
+      modelo: 'modelo3'
+    }
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
---
+++ src/app/componentes/pensamentos/pensamento/pensamento.component.ts
@@ -0,0 +1,28 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pensamento',
+  templateUrl: './pensamento.component.html',
+  styleUrls: ['./pensamento.component.css']
+})
+export class PensamentoComponent implements OnInit {
+
+  @Input() pensamento = {
+    conteudo: 'Sua tarefa é descobrir o seu trabalho e, então, com todo o coração, dedicar-se a ele.',
+    autoria: 'Buda',
+    modelo: 'modelo3'
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  larguraPensamento(): string {
+    if (this.pensamento.conteudo.length >= 256) {
+      return 'pensamento-g'
+    }
+    return 'pensamento-p'
+  }
+
+}
